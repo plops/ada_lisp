84ecff570b942e119e0ea2ff290b31bd77c577c2
wol <wolpumba@gmail.com>
2026-03-25 07:56:37 +0000

    Update collect script to include gnatprove output in file search

--- collect.sh
+++ collect.sh
@@ -1,1 +1,1 @@
-for i in `find  app doc docs proofs scripts src tests *.* -type f`; do echo "# start of "$i;cat $i;done
+for i in `find obj_prove/gnatprove/gnatprove.out app doc docs proofs scripts src tests *.* -type f`; do echo "# start of "$i;cat $i;done
